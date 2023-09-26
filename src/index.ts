1da05796e10210ea75f9df92bbcdba87677801bd
BadCoder1337 <fazebook@rainbow6.ru>
2023-09-26 18:20:11 +0300

    repost forum

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-import { Client, TextChannel, WebhookClient, EmbedBuilder } from "discord.js";
+import { Client, ForumChannel, TextChannel, WebhookClient } from "discord.js";
 import { iterateChannelMessages } from "./iterator";
-import { setTimeout } from "timers/promises";
 
 const client = new Client({
   intents: ["Guilds", "GuildMessageReactions", "GuildMessages"],
+  sweepers: { messages: { lifetime: 3600, interval: 600 } },
 });
 
-const webhook1 = new WebhookClient({ url: process.env.WEBHOOK1 });
-const webhook2 = new WebhookClient({ url: process.env.WEBHOOK2 });
-let i = 0;
-function webhook() {
-  i++;
-  return webhook2;
+function truncate(str: string, n: number, useWordBoundary: boolean) {
+  if (str.length <= n) {
+    return str;
+  }
+  const subString = str.slice(0, n - 1); // the original check
+  return (
+    (useWordBoundary
+      ? subString.slice(0, subString.lastIndexOf(" "))
+      : subString) + "…"
+  );
 }
+
+const webhook = new WebhookClient({ url: process.env.WEBHOOK });
 
 client.login(process.env.DISCORD_TOKEN);
 
@@ -27,37 +33,60 @@ client.on(
 client.on("ready", async () => {
   console.log("Start!", client.user.tag);
   const channel = client.channels.cache.get(process.env.CHANNEL) as TextChannel;
-  for await (const {
-    reactions,
-    author,
-    attachments,
-    content,
-    createdAt,
-  } of iterateChannelMessages(channel)) {
-    await webhook().send({
-      embeds: [
-        new EmbedBuilder()
-          .setAuthor({
-            name: author.globalName,
-            iconURL: author.avatarURL(),
-            url: "https://discord.com/users/" + author.id,
-          })
-          .setDescription(content.replaceAll("|", "") || null)
-          .setTimestamp(createdAt)
-          .addFields({
-            name: "Реакции",
-            value:
-              reactions.cache
-                .map((r) => `${r.emoji} \`${r.count}\``)
-                .join(", ") || "н/д",
-          })
-          .toJSON(),
-      ],
-      threadId: process.env.FORUM_POST,
-      allowedMentions: {},
-      files: [...attachments.values()],
+  const forum = client.channels.cache.get(
+    process.env.FORUM_CHANNEL
+  ) as ForumChannel;
+  const [suggestTag, deniedTag, workingTag, acceptedTag] = [
+    "Предложение",
+    "ОТКЛОНЕНО",
+    "В РАБОТЕ",
+    "ОДОБРЕНО",
+  ].map((t) => forum.availableTags.find((ft) => ft.name === t).id);
+  for await (const message of iterateChannelMessages(channel)) {
+    if (!message.reference?.messageId) continue;
+    const ref = channel.messages.cache.get(message.reference.messageId);
+    if (!ref) continue;
+
+    const title = ref.content
+      .split("\n")
+      .find((v) => v.toLowerCase().startsWith("желаемое изменение"))
+      .replace(/желаемое изменение[: ]*/gi, "");
+    if (!title) continue;
+    const [, denied, working, accepted] =
+      /(отклон)|(в работе)|(одобрено)/gi.exec(message.content);
+    if (!(denied || working || accepted)) continue;
+
+    const post = await webhook.send({
+      content: ref.content,
+      files: [...ref.attachments.values()],
+      avatarURL: ref.author.avatarURL(),
+      username: ref.author.displayName,
+      threadName: truncate(
+        title.charAt(0).toUpperCase() + title.slice(1),
+        95,
+        true
+      ),
     });
-    await setTimeout(1000);
+
+    const thread = forum.threads.cache.get(post.channel_id);
+
+    await webhook.send({
+      content: message.content,
+      files: [...message.attachments.values()],
+      avatarURL: message.author.avatarURL(),
+      username: message.author.displayName,
+      threadId: thread.id,
+    });
+
+    await thread.setAppliedTags(
+      [
+        suggestTag,
+        denied && deniedTag,
+        working && workingTag,
+        accepted && acceptedTag,
+      ].filter(Boolean)
+    );
+    // break;
   }
   console.log("Done!");
 });
--- src/iterator.ts
+++ src/iterator.ts
@@ -1,13 +1,41 @@
-import { TextBasedChannel, Collection, Message } from "discord.js";
+import {
+  type TextBasedChannel,
+  type Collection,
+  type Message,
+} from "discord.js";
+import { DiscordSnowflake } from "@sapphire/snowflake";
 
 const LIMIT = 100;
 
-export async function* iterateChannelMessages(channel: TextBasedChannel) {
-  let after = "0";
-  let bulk: Collection<string, Message<boolean>>;
-  while (after && (!bulk || bulk.size === LIMIT)) {
-    bulk = await channel.messages.fetch({ after, limit: LIMIT });
-    for (const message of bulk.reverse().values()) yield message;
-    after = bulk.lastKey();
+/**
+ * Итерация сообщений канала. `since`
+ * @export
+ * @param {TextBasedChannel} channel
+ * @param {number} [since=0] итерация с заданного сообщения. >=0 - с начала канала, <0 - с конца
+ */
+export async function* iterateChannelMessages(
+  channel: TextBasedChannel,
+  since: number = 0
+) {
+  if (!Number.isInteger(since))
+    throw new RangeError(`${since} is not an integer`);
+  const isForward = since >= 0;
+  let skip = isForward ? since : -since - 1;
+
+  let cursor: string | undefined = DiscordSnowflake.generate({
+    timestamp: Date.UTC(isForward ? 2015 : 3015),
+  }).toString();
+  let bulk: Collection<string, Message<boolean>> | undefined;
+
+  while (cursor && (!bulk || bulk.size === LIMIT)) {
+    bulk = await channel.messages.fetch({
+      [isForward ? "after" : "before"]: cursor,
+      limit: LIMIT,
+    });
+    if (isForward) bulk.reverse();
+    for (const message of bulk.values())
+      if (skip > 0) skip--;
+      else yield message;
+    cursor = bulk.lastKey();
   }
 }
